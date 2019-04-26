5646793c4f9b7933fe4e070e9021d43230933fbc
Karem Jouini <36076266+KaremJouini@users.noreply.github.com>
2019-04-26 10:51:11 +0100

    Add files via upload

---
+++ src/main/java/demoagile/Main.java
@@ -0,0 +1,13 @@
+package demoagile;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		if(3<2)
+			System.out.println("DEAD CODE");
+		
+	}
+
+}
